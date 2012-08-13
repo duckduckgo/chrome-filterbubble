ccefc591a806bf5b66d372ac614618d35135c4e7
mr.Shu <mrshux@gmail.com>
2012-08-13 15:15:51 +0200

    all sorts of improvements

--- js/common.js
+++ js/common.js
@@ -5,21 +5,38 @@
 
 //  }
 
+function getQueryFromURL() {
+    var regex = new RegExp('[\?\&]q=([^\&#]+)');
+    if(regex.test(window.location.href)) {
+        var q = window.location.href.split(regex);
+        q = q[q.length - 2].replace(/\+/g," ");
+
+        return decodeURIComponent(q);
+    }
+}
+
+
 $(document).ready(function(){
     //console.log(results);
-    var query = $("[name='q']").val();
+    var query = getQueryFromURL();
     console.log(query);
     
     getAOLResults(query, function(r){
         
         var cleanResults = [];
 
-        r.children().filter('.result').each(function(){
+        r.each(function(){
             var url = $(this).find('a').attr('href');
-            var matches = url.match(/&s_cu=(.*?)&/);
-
-            cleanResults.push(decodeURIComponent(matches[1]));
+            cleanResults.push(url);
         });
+        
+      //r.children().filter('.result').each(function(){
+      //    var url = $(this).find('a').attr('href');
+      //    var matches = url.match(/&s_cu=(.*?)&/);
+
+      //    cleanResults.push(decodeURIComponent(matches[1]));
+      //});
+
         console.log(cleanResults);
 
         var results = $('#ires li.g:not(#newsbox):not(.noknav)');
@@ -27,21 +44,28 @@ $(document).ready(function(){
         results.each(function(){
             var url = $(this).find('a').attr('href');
             var index = cleanResults.indexOf(url);
-            var span = $('<span>').attr('style', 'color:red;padding-left:5px');
+            var span = $('<span>').attr('style', 'color:red;font-size:small;padding-left:5px');
 
             if (index != -1) {
                 if (index != iter) {
-                    if (index > iter) {
-                        span.html('&#9650;');
-                    } else {
-                        span.html('&#9660;');
-                    }
+                    span.html('#' + (index + 1) + ' &#10132; ' + '#' + (iter + 1));
+                  //if (index > iter) {
+                  //    span.html('&#9650;');
+                  //} else {
+                  //    span.html('&#9660;');
+                  //}
                 }
-                iter += 1;
+                $(this).find('h3').append(span);
             } else {
-                span.append($('<small>').text('Added result'));
+                var div = $('<div>').css({
+                            'background-image': 'url(http://duckduckgo.com/assets/icon_plus.v103.png)',
+                            'height': '16px',
+                            'width': '16px',
+                            'float': 'left'
+                            });
+                $(this).find('h3').prepend(div);
             }
-            $(this).find('h3').append(span);
+            iter += 1;
         });
     });
 });
@@ -49,12 +73,18 @@ $(document).ready(function(){
 
 function getAOLResults(query, callback) {
     var req = new XMLHttpRequest();
-    req.open('GET', 'http://m.aol.com/search/default/search.do?query=' + encodeURIComponent(query) + '&invocationType=topsearchbox.waphome&sources=google_websearch&first=0', true);
+    //req.open('GET', 'http://search.aol.com/aol/search?enabled_terms=&count_override=200&s_it=comsearch51&q=' + encodeURIComponent(query), true);
+    var url = 'http://search.aol.com/aol/search?enabled_terms=&s_it=comsearch51&q=' + encodeURIComponent(query);
+    console.log(url);
+    req.open('GET', url, true);
 
     req.onreadystatechange = function(data) {
         if (req.readyState != 4)  { return; } 
+        console.log('response:', req.responseText);
         var r = $('div', req.responseText);
-        r = r.filter('.resultlist:first');
+
+        r = r.find('.MSL li[about="null"]');
+        console.log(r);
         callback(r);
     }
 
