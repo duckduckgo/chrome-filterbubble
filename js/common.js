a7c2b001ec4f6b36a449f02c9c531cc99322536f
mr.Shu <mr@shu.io>
2012-10-16 22:26:28 +0200

    adding results which were not present at Google

--- js/common.js
+++ js/common.js
@@ -24,10 +24,25 @@ $(document).ready(function(){
     getAOLResults(query, function(r){
         
         var cleanResults = [];
+        var cleanResultsData = [];
+        var dirtyResults = [];
 
         r.each(function(){
             var url = $(this).find('a').attr('href');
+            var title = $(this).find('a').html();
+            var desc = $(this).find('p:not(.find)').html();
+
             cleanResults.push(url);
+            cleanResultsData.push({url:url, title:title, desc:desc});
+        });
+
+
+        var results = $('#ires li.g:not(#newsbox):not(.noknav)');
+        results.each(function(){
+            var url = $(this).find('a').attr('href');
+
+            if (url.indexOf('http') !== -1)
+                dirtyResults.push(url);
         });
         
       //r.children().filter('.result').each(function(){
@@ -38,11 +53,15 @@ $(document).ready(function(){
       //});
 
         console.log(cleanResults);
+        console.log(dirtyResults);
 
-        var results = $('#ires li.g:not(#newsbox):not(.noknav)');
+
         var iter = 0;
         results.each(function(){
             var url = $(this).find('a').attr('href');
+            if (url.indexOf('http') === -1)
+                return;
+
             var index = cleanResults.indexOf(url);
             var span = $('<span>').css({
                 'color': 'red',
@@ -82,11 +101,52 @@ $(document).ready(function(){
               $(this).find('h3').prepend(span);
 
             }
+
+            //console.log(cleanResults.indexOf(dirtyResults[iter]), dirtyResults[iter]);
+
+            if (dirtyResults.indexOf(cleanResults[iter]) === -1) {
+                
+                // adds generated google result
+                $(this).after(generateGoogleResult(cleanResultsData[iter]));
+                //console.log(iter, cleanResults[iter], cleanResultsData[iter]);
+                //console.log(generateGoogleResult(cleanResultsData[iter]));
+
+            }
+
             iter += 1;
+
+
+
         });
     });
 });
 
+
+function generateGoogleResult(r) {
+    var span = $('<span>').css({
+                'color': 'red',
+                'padding-right': '5px',
+                'font-size': 'small'
+            });
+    span.css({ 
+                'color': '#b8b8b8',
+                'font-size': 'x-large',
+                'font-weight': 'bold',
+                'padding': '0 3px 0 0'
+              });
+    span.html('-');
+ 
+    var resultDiv = $('<div>').attr('class', 'vsc');
+    resultDiv.append($('<h3>').prepend(span).append(
+                        $('<a>').attr({href: r.url, class: 'l'})
+                                .html(r.title)));
+    resultDiv.append($('<div>').attr('class', 's').append(
+                        $('<div>').attr('class', 'f kv').append(
+                            $('<cite>').html(r.url))).append(
+                        $('<span>').attr('class', 'st').html(r.desc))
+                    );
+    return $('<li>').attr('class', 'g').append(resultDiv);
+}
 
 function getAOLResults(query, callback) {
     var req = new XMLHttpRequest();
